33c7e5e345436b7a9be60c6e55be2f4d6cbed577
Dylan Cherry <dylancherry98@gmail.com>
2022-12-11 18:23:04 -0500

    Adsded name (logo) to the left of navbar, slightly altered horizontal alignment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,14 @@ import "./Navbar.css"
 export default function Navbar() {
   return (
   <div className="navbar">
-    <a href="/" alt="Home">Home</a>
-    <a href="/" alt="About">About</a>
-    <a href="/" alt="Contact">Contact</a>
+    <div className='logo'>
+      <h4>Dylan Cherry</h4>
+    </div>
+    <div className="navbar-links">
+      <a href="/" alt="Home">Home</a>
+      <a href="/" alt="About">About</a>
+      <a href="/" alt="Contact">Contact</a>
+    </div>
   </div>
   )
 }
