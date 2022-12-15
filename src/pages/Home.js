641a1b6a1236d796ba7b3ee633b08a5a7fd207ef
Dylan Cherry <dylancherry98@gmail.com>
2022-12-14 21:47:27 -0500

    finished styling prototype of Home component on modbile and desktop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 // styles
 import "./Home.scss"
 
+// images
+import hero from "../assets/hero.png"
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div class="main-text">
+      <img src={hero} alt="Portrait of Dylan"/>
+      <p>Hi, I'm <span>Dylan Cherry</span>!</p><p>I'm a <span>full-stack developer<span/></span>.</p>
+    </div>
   )
 }
