1d96fb079a080a483b16fc8550ae62f4a56f41aa
Dylan Cherry <dylancherry98@gmail.com>
2022-12-15 21:40:00 -0500

    installed react-switch, imported useState and useContext, began setting up logic for dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
+import { createContext, useContext, useState } from 'react';
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
-    <div className="App">
-      <Navbar />
+    <ThemeContext.Provider value = {{ theme, toggleTheme }}>
+    <div className="App" id={theme}>
+      <Navbar/>
       <Home />
     </div>
+    </ThemeContext.Provider>
   );
 }
 
