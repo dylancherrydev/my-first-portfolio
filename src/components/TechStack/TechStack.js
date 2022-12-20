f92ef5c05f93a9f4b0a566fec1b5b573ca09c83e
Dylan Cherry <dylancherry98@gmail.com>
2022-12-20 08:29:21 -0500

    added text to each subsection of tech stack

--- src/components/TechStack/TechStack.js
+++ src/components/TechStack/TechStack.js
@@ -1,4 +1,4 @@
-// current relevant tech stack = js, css, html, react, git, sql, nodeJS, scss, firebase
+// current relevant tech stack = js, css, html, react, git, sql, nodeJS, sass, firebase
 
 import "./TechStack.scss"
 
@@ -15,34 +15,42 @@ export default function TechStack() {
           </div>
           <div className="ts-item ts-css">
             <div className="ts-box">
+              <p>CSS 3</p>
             </div>
           </div>
           <div className="ts-item ts-sql">
             <div className="ts-box">
+              <p>SQL</p>
             </div>
           </div>
           <div className="ts-item ts-react">
             <div className="ts-box">
+              <p>React</p>
             </div>
           </div>
           <div className="ts-item ts-node">
             <div className="ts-box">
+              <p>Node</p>
             </div>
           </div>
           <div className="ts-item ts-js">
             <div className="ts-box">
+              <p>JavaScript ES6</p>
             </div>
           </div>
           <div className="ts-item ts-firebase">
             <div className="ts-box">
+              <p>Firebase</p>
             </div>
           </div>
           <div className="ts-item ts-html">
             <div className="ts-box">
+              <p>HTML 5</p>
             </div>
           </div>
-          <div className="ts-item ts-scss">
+          <div className="ts-item ts-sass">
             <div className="ts-box">
+              <p>Sass</p>
             </div>
           </div>
         </div>
