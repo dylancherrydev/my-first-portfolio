03fefc3b1fb0817aa08601a59cad47ad6d676aec
Dylan Cherry <dylancherry98@gmail.com>
2022-12-26 22:14:09 -0500

    Finished styling and building temporary Projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,15 @@
+import { useTheme } from "../../hooks/useTheme"
+
+// styles
+import "./Projects.scss"
+
 export default function Projects() {
+  const darkness = useTheme()
+
   return (
-    <div>Projects</div>
+    <div className={`container-projects ${darkness}`}>
+      <h1 className="section-title">Projects</h1>
+      <h1 className="coming-soon">Coming soon...</h1>
+    </div>
   )
 }
