77b04bf4a463f4543d57b3bb125309d0f7fc64d5
Dylan Cherry <dylancherry98@gmail.com>
2022-12-26 22:17:59 -0500

    added smooth scroll functionality for Projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,7 +7,7 @@ export default function Projects() {
   const darkness = useTheme()
 
   return (
-    <div className={`container-projects ${darkness}`}>
+    <div className={`container-projects ${darkness}`} id="scroll-projects">
       <h1 className="section-title">Projects</h1>
       <h1 className="coming-soon">Coming soon...</h1>
     </div>
